ff8b1acd53c5113181e14b927eb6a25632297e72
Tim Garlick <tim.garlick@Garlicks-Mac.local>
2016-10-03 17:09:17 -0600

    need to figure out adding and removing class issue

--- js/actions.js
+++ js/actions.js
@@ -1,30 +1,35 @@
 $(function ($) {
 
-	var test = null;
+	var start = true;
+	var questionNumber = $('.pagination-container li');
 
+	var pageLoad = function() {
+		if (start === true) {
+			$("#industry").show();
+			$(".start").css('background', '#004F8C').css('color', '#ffffff');
+		}
+	}
+	pageLoad();
 
-	$( "form" ).on( "submit", function( event ) {
-		event.preventDefault();
-		// console.log( $( this ).serialize() );
-		console.log(formData);
+	questionNumber.on( 'click', function() {
+		// var filterValue = $( this ).attr('data-filter');
+		start = false;
 
-		// test = $( this ).serializeArray();
-		// console.log(test);
-		// if(test) {
-		// 	console.log("worked");
-		// 	formLoop(test);
-		// }
+		questionNumber.not(this).css('background', '#ffffff').css('color', '#004F8C');
+		$(this).css('background', '#004F8C').css('color', '#ffffff');
+		var filterValue = $(this).data('filter');
+		var filterId = '#' + filterValue;
+		console.log(filterId);
+		var test1 = $(filterId).attr("id", filterValue).fadeIn(300);
+		console.log(test1);
+
+		// $('#' + filterValue).fadeIn(1000);
+
 	});
 
+	var filter = function(filterValue) {
+		questionNumber.not(this)
 
-
-	var formData = {};
-
-	formData.industry = $('input ').val();
-	formData.company_size = $('input .company-size').val();
-	formData.users = $('input .users').val();
-	// formData.system = $('input').data('system');
-	// formData.timframe = $('input').data('timeframe');
-	// formData.discount_code = $('input[type=["text"]).val();
+	}
 
 })
