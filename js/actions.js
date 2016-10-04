62928ad8ecaec6e6beab08f9020397ce50afe6da
Tim Garlick <tim.garlick@Garlicks-Mac.local>
2016-10-04 11:36:16 -0600

    figured out some filtering options

--- js/actions.js
+++ js/actions.js
@@ -2,6 +2,7 @@ $(function ($) {
 
 	var start = true;
 	var questionNumber = $('.pagination-container li');
+	var form = $(form);
 
 	var pageLoad = function() {
 		if (start === true) {
@@ -12,24 +13,17 @@ $(function ($) {
 	pageLoad();
 
 	questionNumber.on( 'click', function() {
-		// var filterValue = $( this ).attr('data-filter');
 		start = false;
 
 		questionNumber.not(this).css('background', '#ffffff').css('color', '#004F8C');
 		$(this).css('background', '#004F8C').css('color', '#ffffff');
+		
 		var filterValue = $(this).data('filter');
 		var filterId = '#' + filterValue;
-		console.log(filterId);
-		var test1 = $(filterId).attr("id", filterValue).fadeIn(300);
-		console.log(test1);
-
-		// $('#' + filterValue).fadeIn(1000);
+		var currentFilter = $(filterId).attr("id", filterValue).fadeIn(200);
+		
+		form.children().not("#" + currentFilter[0].id).hide(-500);
 
 	});
 
-	var filter = function(filterValue) {
-		questionNumber.not(this)
-
-	}
-
-})
+});
