3ee5b13dc1c28b4f547911db2683d5b3bf896ce9
Tim Garlick <tim.garlick@Garlicks-Mac.local>
2016-10-05 16:06:06 -0600

    most of the filtering is done

--- js/actions.js
+++ js/actions.js
@@ -1,7 +1,7 @@
 $(function ($) {
 
 	var start = true;
-	var questionNumber = $('.pagination-container li');
+	var questionNumber = $('.pagination-container button');
 	var form = $('form');
 
 	var pageLoad = function() {
@@ -26,98 +26,50 @@ $(function ($) {
 		form.children().not(currentFilterId).hide(-500);
 
 	});
+	var buttonState = function() {
+
+	}
+	$(".showNext").prop("disabled", true).css('opacity', '.5');
+
+	$("input:radio").change(function (e) { 
+		$(this).parent('label').siblings('.showNext').prop('disabled', false).css('opacity', '1');
+
+	});
+	$("input:checkbox").click(function (e) { 
+		var opacity;
+		var buttonState = $('input:checkbox:checked').length;
+
+		if (buttonState) {
+			opacity = '1';			
+		}
+		else {opacity = '.5'}
+			$(this).parent('label').siblings('.showNext').prop('disabled', !$('input:checkbox:checked').length).css('opacity', opacity);
+
+	});
 
 	$('.showPrevious').click(function(e) {
-		var previousQuestion = e.currentTarget.parentElement.previousElementSibling.id
+		var previousQuestion = e.currentTarget.parentElement.previousElementSibling.id;
 		var previousQuestionId = "#" + e.currentTarget.parentElement.previousElementSibling.id;
+		
 		$(previousQuestionId).attr("id", previousQuestion).fadeIn(200);
 		form.children().not(previousQuestionId).hide(-500);
-	})
+
+		questionNumber.removeClass('blue');
+		$('.pagination-container button[data-filter=' + previousQuestion + ']').addClass('blue');
+	});
+
+	$('.showNext').click(function(e) {
+		var nextQuestion = e.currentTarget.parentElement.nextElementSibling.id;
+		var nextQuestionId = "#" + e.currentTarget.parentElement.nextElementSibling.id;
+		
+		$(nextQuestionId).attr("id", nextQuestion).fadeIn(200);
+		form.children().not(nextQuestionId).hide(-500);
+
+		questionNumber.removeClass('blue');
+		$('.pagination-container button[data-filter=' + nextQuestion + ']').addClass('blue');
+
+	});
 
 });
 
 
-
-// +var r = 0;
-//  +var g = 96;
-//  +var b = 112;
-//  +
-//  +$(document).ready(function ()
-//  +{
-//  +
-//  +    
-//  +    var pages = [1, 2, 3,4,5,6,7,8];
-//  +
-//  +    for (var i = pages.length - 1; i >= 0 ; i--)
-//  +    {
-//  +
-//  +        var builder = buildPage(i, pages);
-//  +        if (i == 0)
-//  +        {
-//  +            builder.addClass("selectedSmall");
-//  +        }
-//  +
-//  +    }
-//  +});
-//  +
-//  +function showNext(div)
-//  +{
-//  +    $(".accordion").removeClass("selectedSmall", 300);
-//  +   
-//  +
-//  +    
-//  +        var a = div.prev().hasClass("selectedSmall", 300);
-//  +
-//  +        if (!a) // if the current target doesnt have the class add it. if it does do nothing;
-//  +        {
-//  +            div.prev().addClass("selectedSmall", 300);
-//  +           
-//  +           
-//  +
-//  +
-//  +        }
-//  +
-//  +}
-//  +function showPrev(div)
-//  +{
-//  +    $(".accordion").removeClass("selectedSmall", 300);
-//  +   
-//  +    
-//  +
-//  +    var a = div.next().hasClass("selectedSmall", 300);
-//  +
-//  +    if (!a) // if the current target doesnt have the class add it. if it does do nothing;
-//  +    {
-//  +        div.next().addClass("selectedSmall", 300);
-//  +       
-//  +        
-//  +
-//  +
-//  +    }
-//  +
-//  +}
-//  +
-//  +function buildPage(i, pages)
-//  +{
-//  +    
-//  +    var rbg = "rgb(" + r + ", " + g + ", " + b + ")";
-//  +
-//  +    var accordionStep = $("<div>").appendTo($(".accordionContainer")).addClass("accordion").html(pages[i]).css("background", rbg).prop("id", "page" + i);
-//  +
-//  +    // $("<div>").appendTo($(accordionStep)).addClass("fa fa-times-circle-o fa-2x").css({ "left": "49%" });
-//  +    if (i < (pages.length -1)) $("<div>").appendTo($(accordionStep)).addClass("fa fa-arrow-circle-o-right fa-3x").click(function (e)
-//  +    {
-//  +        $(".accordion").removeClass("fa")
-//  +        showNext(accordionStep);
-//  +    });
-//  +    if (i !== 0) $("<div>").appendTo($(accordionStep)).addClass("fa fa-arrow-circle-o-left fa-3x").click(function (e)
-//  +    {
-//  +
-//  +        showPrev(accordionStep);
-//  +    });
-//  +
-//  +    b = b + (pages.length + 12);
-//  +    g = g + (pages.length + 43);
-//  +    r = r + (pages.length + 65);
-//  +    return accordionStep;
-//  +} 
