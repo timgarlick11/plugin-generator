92764f8b767cd13f92e70d326ed0c26c893245b3
Tim Garlick <tim.garlick@Garlicks-Mac.local>
2016-10-07 17:59:12 -0600

    going to add a progress bar to make multi routing easier

--- js/actions.js
+++ js/actions.js
@@ -3,6 +3,7 @@ $(function ($) {
 	var start = true;
 	var questionNumber = $('.pagination-container button');
 	var form = $('form');
+	var checkboxChecked = 0;
 
 	$(".showNext").prop("disabled", true).css('opacity', '.5');
 	$(".pagination-container button").prop("disabled", true).css('opacity', '.5');//set all buttons to disabled on page load
@@ -10,7 +11,7 @@ $(function ($) {
 	var pageLoad = function() {
 		if (start === true) {
 			$("#industry").show();
-			$('.start').addClass('blue').prop('disabled', false).css('opacity', '1');
+			$('.start').addClass('highlight').prop('disabled', false).css('opacity', '1');
 		}
 	}
 	pageLoad();
@@ -32,6 +33,8 @@ $(function ($) {
 
 
 
+
+
 	$("input:radio").change(function (e) { 
 		$(this).parent('label').siblings('.showNext').prop('disabled', false).css('opacity', '1');
 		
@@ -47,22 +50,27 @@ $(function ($) {
 	$("input:checkbox").click(function (e) { 
 		var opacity;
 		var buttonState = $('input:checkbox:checked').length;//returns boolean. If there is a length it is true
+		console.log(buttonState)
 
 
-		if (buttonState) {
-			opacity = '1';			
+		if (buttonState > 0) {
+			opacity = '1';
+			checkboxChecked++;			
 		}
-		else {opacity = '.5'}
-			
-			$(this).parent('label').siblings('.showNext').prop('disabled', !$('input:checkbox:checked').length).css('opacity', opacity);
+		else {
+			opacity = '.5'; 
+			checkboxChecked--;
+		}
 
-		var numberFilter = $(this).parent('label').parent().data('number');
-		var allButtons = $(this).parents().find('.pagination-container').children('button').eq(numberFilter - 1);
+		$(this).parent('label').siblings('.showNext').prop('disabled', !$('input:checkbox:checked').length).css('opacity', opacity);
+
+		// var numberFilter = $(this).parent('label').parent().data('number');
+		// var allButtons = $(this).parents().find('.pagination-container').children('button').eq(numberFilter - 1);
 		
-		allButtons.each(function(index, element) {
-			$(this).next().prop('disabled', !$('input:checkbox:checked')).css('opacity', opacity);
+		// allButtons.each(function(index, element) {
+		// 	$(this).next().prop('disabled', !$('input:checkbox:checked').length).css('opacity', opacity);
 
-		});
+		// });
 
 	});
 
@@ -70,8 +78,8 @@ $(function ($) {
 		var previousQuestion = $(this).parent('.input-seperator').prev().fadeIn(500);
 		
 		form.children().not(previousQuestion).fadeOut(-500);
-		questionNumber.removeClass('blue');
-		$('.pagination-container button[data-filter=' + previousQuestion[0].id + ']').addClass('blue');
+		questionNumber.removeClass('highlight');
+		$('.pagination-container button[data-filter=' + previousQuestion[0].id + ']').addClass('highlight');
 	});
 
 	$('.showNext').click(function(e) {
@@ -79,9 +87,33 @@ $(function ($) {
 		console.log(nextQuestion);
 		
 		form.children().not(nextQuestion).fadeOut(-500);
-		questionNumber.removeClass('blue');
-		$('.pagination-container button[data-filter=' + nextQuestion[0].id + ']').addClass('blue');
+		questionNumber.removeClass('highlight');
+		$('.pagination-container button[data-filter=' + nextQuestion[0].id + ']').addClass('highlight');
+		// var formLength = $("form input[required]").length
+		// console.log(formLength)
+		formProgress();
+
+
 
 	});
+
+	
+	var formProgress = function() {
+		var validInputNum = 0;
+		$("form input[required").each(function(index, element) {
+			console.log(this.validity.valid);
+			if (this.validity.valid) {
+				validInputNum++;
+			}
+			
+
+
+		});
+		var checkboxLength = $(".checked").length // work around since required on checkboxes have a lot of issues
+		var formLength = $("form input[required").length;
+		var progress = Math.round((validInputNum + checkboxChecked) /(formLength + checkboxLength) * 100);
+		
+		var test = $('.progress-bar').css('width', progress + '%');
+	}
 
 });
